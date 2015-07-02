c06ffcbedb91e19c29ea30916b53cf9e076ba353
Smit Patel <smitpatel@users.noreply.github.com>
2015-07-02 14:29:30 -0700

    More DataAnnotations NotMappedAttribute MaxLengthAttribute StringLengthAttribute TimestampAttribute

--- test/EntityFramework.Core.Tests/Metadata/ModelConventions/PropertyAttributeConventionTest.cs
+++ test/EntityFramework.Core.Tests/Metadata/ModelConventions/PropertyAttributeConventionTest.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using Microsoft.Data.Entity.Internal;
 using Microsoft.Data.Entity.Metadata.Conventions.Internal;
 using Microsoft.Data.Entity.Metadata.Internal;
@@ -96,47 +97,6 @@ namespace Microsoft.Data.Entity.Metadata.Conventions
 
         #endregion
 
-        #region RequiredAttribute
-
-        [Fact]
-        public void RequiredAttribute_overrides_configuration_from_convention_source()
-        {
-            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
-
-            var propertyBuilder = entityBuilder.Property(typeof(string), "Name", ConfigurationSource.Explicit);
-
-            propertyBuilder.Required(false, ConfigurationSource.Convention);
-
-            new RequiredAttributeConvention().Apply(propertyBuilder);
-
-            Assert.False(propertyBuilder.Metadata.IsNullable);
-        }
-
-        [Fact]
-        public void RequiredAttribute_does_not_override_configuration_from_explicit_source()
-        {
-            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
-
-            var propertyBuilder = entityBuilder.Property(typeof(string), "Name", ConfigurationSource.Explicit);
-
-            propertyBuilder.Required(false, ConfigurationSource.Explicit);
-
-            new RequiredAttributeConvention().Apply(propertyBuilder);
-
-            Assert.True(propertyBuilder.Metadata.IsNullable);
-        }
-
-        [Fact]
-        public void RequiredAttribute_sets_is_nullable_with_conventional_builder()
-        {
-            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
-            var entityTypeBuilder = modelBuilder.Entity<A>();
-
-            Assert.False(entityTypeBuilder.Property(e => e.Name).Metadata.IsNullable);
-        }
-
-        #endregion
-
         #region KeyAttribute
 
         [Fact]
@@ -204,6 +164,218 @@ namespace Microsoft.Data.Entity.Metadata.Conventions
 
         #endregion
 
+        #region MaxLengthAttribute
+
+        [Fact]
+        public void MaxLengthAttribute_overrides_configuration_from_convention_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "MaxLengthProperty", ConfigurationSource.Explicit);
+
+            propertyBuilder.MaxLength(100, ConfigurationSource.Convention);
+
+            new MaxLengthAttributeConvention().Apply(propertyBuilder);
+
+            Assert.Equal(10, propertyBuilder.Metadata.GetMaxLength());
+        }
+
+        [Fact]
+        public void MaxLengthAttribute_does_not_override_configuration_from_explicit_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "MaxLengthProperty", ConfigurationSource.Explicit);
+
+            propertyBuilder.MaxLength(100, ConfigurationSource.Explicit);
+
+            new MaxLengthAttributeConvention().Apply(propertyBuilder);
+
+            Assert.Equal(100, propertyBuilder.Metadata.GetMaxLength());
+        }
+
+        [Fact]
+        public void MaxLengthAttribute_set_max_length_with_conventional_builder()
+        {
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            var entityTypeBuilder = modelBuilder.Entity<A>();
+
+            Assert.Equal(10, entityTypeBuilder.Property(e => e.MaxLengthProperty).Metadata.GetMaxLength());
+        }
+
+        #endregion
+
+        #region NotMappedAttribute
+
+        [Fact]
+        public void NotMappedAttribute_overrides_configuration_from_convention_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            Assert.NotNull(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Convention));
+
+            new NotMappedPropertyAttributeConvention().Apply(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Convention));
+
+            Assert.Null(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Convention));
+        }
+
+        [Fact]
+        public void NotMappedAttribute_does_not_override_configuration_from_explicit_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            Assert.NotNull(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Convention));
+
+            new NotMappedPropertyAttributeConvention().Apply(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Explicit));
+
+            Assert.NotNull(entityBuilder.Property(typeof(string), "IgnoredProperty", ConfigurationSource.Convention));
+        }
+
+        [Fact]
+        public void NotMappedAttribute_ignores_property_with_conventional_builder()
+        {
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            var entityTypeBuilder = modelBuilder.Entity<A>();
+
+            Assert.False(entityTypeBuilder.Metadata.Properties.Any(p => p.Name == "IgnoredProperty"));
+        }
+
+        #endregion
+
+        #region RequiredAttribute
+
+        [Fact]
+        public void RequiredAttribute_overrides_configuration_from_convention_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "Name", ConfigurationSource.Explicit);
+
+            propertyBuilder.Required(false, ConfigurationSource.Convention);
+
+            new RequiredAttributeConvention().Apply(propertyBuilder);
+
+            Assert.False(propertyBuilder.Metadata.IsNullable);
+        }
+
+        [Fact]
+        public void RequiredAttribute_does_not_override_configuration_from_explicit_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "Name", ConfigurationSource.Explicit);
+
+            propertyBuilder.Required(false, ConfigurationSource.Explicit);
+
+            new RequiredAttributeConvention().Apply(propertyBuilder);
+
+            Assert.True(propertyBuilder.Metadata.IsNullable);
+        }
+
+        [Fact]
+        public void RequiredAttribute_sets_is_nullable_with_conventional_builder()
+        {
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            var entityTypeBuilder = modelBuilder.Entity<A>();
+
+            Assert.False(entityTypeBuilder.Property(e => e.Name).Metadata.IsNullable);
+        }
+
+        #endregion
+
+        #region StringLengthAttribute
+
+        [Fact]
+        public void StringLengthAttribute_overrides_configuration_from_convention_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "StringLengthProperty", ConfigurationSource.Explicit);
+
+            propertyBuilder.MaxLength(100, ConfigurationSource.Convention);
+
+            new StringLengthAttributeConvention().Apply(propertyBuilder);
+
+            Assert.Equal(20, propertyBuilder.Metadata.GetMaxLength());
+        }
+
+        [Fact]
+        public void StringLengthAttribute_does_not_override_configuration_from_explicit_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "StringLengthProperty", ConfigurationSource.Explicit);
+
+            propertyBuilder.MaxLength(100, ConfigurationSource.Explicit);
+
+            new StringLengthAttributeConvention().Apply(propertyBuilder);
+
+            Assert.Equal(100, propertyBuilder.Metadata.GetMaxLength());
+        }
+
+        [Fact]
+        public void StringLengthAttribute_set_max_length_with_conventional_builder()
+        {
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            var entityTypeBuilder = modelBuilder.Entity<A>();
+
+            Assert.Equal(20, entityTypeBuilder.Property(e => e.StringLengthProperty).Metadata.GetMaxLength());
+        }
+
+        #endregion
+
+        #region TimestampAttribute
+
+        [Fact]
+        public void TimestampAttribute_overrides_configuration_from_convention_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(byte[]), "Timestamp", ConfigurationSource.Explicit);
+
+            propertyBuilder.ConcurrencyToken(false, ConfigurationSource.Convention);
+
+            new TimestampAttributeConvention().Apply(propertyBuilder);
+
+            Assert.True(propertyBuilder.Metadata.IsConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampAttribute_does_not_override_configuration_from_explicit_source()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<A>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(byte[]), "Timestamp", ConfigurationSource.Explicit);
+
+            propertyBuilder.ConcurrencyToken(false, ConfigurationSource.Explicit);
+
+            new TimestampAttributeConvention().Apply(propertyBuilder);
+
+            Assert.False(propertyBuilder.Metadata.IsConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampAttribute_sets_concurrency_token_with_conventional_builder()
+        {
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            var entityTypeBuilder = modelBuilder.Entity<A>();
+
+            Assert.True(entityTypeBuilder.Property(e => e.RowVersion).Metadata.IsConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampAttribute_throws_if_used_on_non_binary_property()
+        {
+            var entityBuilder = CreateInternalEntityTypeBuilder<C>();
+
+            var propertyBuilder = entityBuilder.Property(typeof(string), "Timestamp", ConfigurationSource.Explicit);
+
+            Assert.Equal(Strings.TimestampAttributeOnNonBinary("Timestamp"),
+                Assert.Throws<InvalidOperationException>(() => new TimestampAttributeConvention().Apply(propertyBuilder)).Message);
+        }
+
+        #endregion
+
         private InternalEntityTypeBuilder CreateInternalEntityTypeBuilder<T>()
         {
             var conventionSet = new ConventionSet();
@@ -227,6 +399,18 @@ namespace Microsoft.Data.Entity.Metadata.Conventions
 
             [Key]
             public int MyPrimaryKey { get; set; }
+
+            [NotMapped]
+            public string IgnoredProperty { get; set; }
+
+            [MaxLength(10)]
+            public string MaxLengthProperty { get; set; }
+
+            [StringLength(20)]
+            public string StringLengthProperty { get; set; }
+
+            [Timestamp]
+            public byte[] Timestamp { get; set; }
         }
 
         public class B
@@ -236,6 +420,17 @@ namespace Microsoft.Data.Entity.Metadata.Conventions
 
             [Key]
             public int MyPrimaryKey { get; set; }
+        }
+
+        public class C
+        {
+            [Key]
+            public int Id { get; set; }
+
+            public string Data { get; set; }
+
+            [Timestamp]
+            public string Timestamp { get; set; }
         }
 
         public class MyContext : DbContext
